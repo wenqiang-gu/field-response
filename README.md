90ea0e347272803238c6502adfec56606db3c149
Wenqiang Gu <goowenq@gmail.com>
2021-03-30 16:09:35 -0400

    add icarus field response

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # field-response
-Here we can get some field response files for LArTPC in ROOT format 
+Here are some field response files for LArTPC in json format, and a python script for converting json format to ROOT TH2 histogram
+
+* garfield-icarus-fnal-commissioning.[json.bz2/root]
+
+Garfield simulation for ICARUS: -240V, +10V, +260V for Y,U,V planes
+Note that in ICARUS, Y is the first induction, U is the second induction, and V is the collection
+
+The normalization is determined from the Garfield simulation. In a full transparency condition,
+if you take integral of the central electron path, the integral should be close to 0.01 e/ns.
+As the time binning is 100ns in the histogram, it's equivalent to 1 electron.
+
+* Garfield simulation condition for SBND (in progress):
+1st induction: -200V
+2nd induction: 0V
+collection:    430V 
+
---
+++ avg_fr_json2root.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+import sys
+filename = sys.argv[1]
+print(filename)
+
+import bz2
+import json
+import ROOT
+
+# time vs wire number
+# time has unit of us
+# icarus assumes three planes in Y, U, V
+hfr = [
+ ROOT.TH2F("FieldRes_Y","Y plane (1st ind.) unit: electrons/ns",210, -10.5-0.05, 10.5-0.05, 1000,-60,40 )
+,ROOT.TH2F("FieldRes_U","U plane (2nd ind.) unit: electrons/ns",210, -10.5-0.05, 10.5-0.05, 1000,-60,40 )
+,ROOT.TH2F("FieldRes_V","V plane (col.) unit: electrons/ns",210, -10.5-0.05, 10.5-0.05, 1000,-60,40 )
+]
+
+with bz2.BZ2File(filename) as bzinput:
+  obj = json.load(bzinput)
+  for iplane in [0,1,2]:
+    hfr[iplane].GetXaxis().SetTitle("Wire Number")
+    hfr[iplane].GetYaxis().SetTitle("Time [#mus]")
+    
+    wire_pitch = obj["FieldResponse"]["planes"][iplane]["PlaneResponse"]["pitch"]
+
+    for ipitch in range(126):
+      fr_array = obj["FieldResponse"]["planes"][iplane]["PlaneResponse"]["paths"][ipitch]["PathResponse"]["current"]["array"]["elements"]
+      path_position = obj["FieldResponse"]["planes"][iplane]["PlaneResponse"]["paths"][ipitch]["PathResponse"]["pitchpos"]
+      ipos1 = hfr[iplane].GetXaxis().FindBin(path_position / wire_pitch)
+      for itick in range(1000):
+        hfr[iplane].SetBinContent(ipos1, itick+1, -fr_array[itick])
+        ipos2 = hfr[iplane].GetXaxis().FindBin(-path_position / wire_pitch)
+        if hfr[iplane].GetBinContent(ipos2, itick+1)>0:
+          print("ignore bin %d" %ipos2)
+        else:
+          hfr[iplane].SetBinContent(ipos2, itick+1, -fr_array[itick])
+        
+     
+#hfr[1].Draw("colz")
+
+ofile = ROOT.TFile(filename[:-9]+".root","recreate") # assume filename ends with ".json.bz2"
+hfr[0].Write()
+hfr[1].Write()
+hfr[2].Write()
+ofile.Close()
+
+# average over wire pitch (1 wire = 10 pitchs)
+# hfravg = [
+#  ROOT.TH2F("FieldResponse_U","U plane", 1000,0,1000, 21, -106,104)
+# ,ROOT.TH2F("FieldResponse_V","V plane", 1000,0,1000, 21, -106,104)
+# ,ROOT.TH2F("FieldResponse_W","W plane", 1000,0,1000, 21, -106,104)
+# ]
+# 
+# for iplane in range(3):
+#   for i in range(1000):
+#     for j in range(21):
+#       content =0
+#       for idx in range(j*10+1, j*10+11):
+#         content += hfr[iplane].GetBinContent(idx,i+1) 
+#       content *= 0.1
+# 
+#       hfravg[iplane].SetBinContent(i+1, j+1, content)
+# 
+# ofile = ROOT.TFile(filename[:-9]+".root","recreate") # assume filename ends with ".json.bz2"
+# hfravg[0].Write()
+# hfravg[1].Write()
+# hfravg[2].Write()
+# ofile.Close()
+
